5420ad37917f759f7fc4c55b8c72c8023415bd6a
Qwizi <500adrian2@wp.pl>
2019-09-21 12:10:40 +0200

    Set user model name unique

--- backend/quiz/user/models.py
+++ backend/quiz/user/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
