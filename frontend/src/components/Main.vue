93bddf751b3e5b8394e526b6e711827044874f19
Poftorek <Poftorek@o2.pl>
2019-10-02 22:23:58 +0200

    Add stores data in localStorage after POST query

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,10 +1,17 @@
 <template>
   <main class="main">
     <div class="welcome">
-      <form class="nick-form">
+      <form class="nick-form" v-if='!isUserLogged()'>
         <label for="nick" class="label">
           Wpisz swój nick:
-          <input type="text" id="nick" name="nick" placeholder="Nick" class="input" v-model="nick" />
+          <input
+            type="text"
+            id="nick"
+            name="nick"
+            placeholder="Nick"
+            class="input"
+            v-model="nick"
+          />
         </label>
         <button class="btn btn--ok" @click="handleClick">OK</button>
       </form>
@@ -13,20 +20,43 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      nick: ''
-    }
+      nick: ""
+    };
   },
   methods: {
+    isUserLogged: function(e) {
+      if (localStorage.getItem("name")) {
+        return 1;
+      } else {
+        return 0;
+      }
+    },
+
+    userAddedOK: function(e) {
+      localStorage.setItem("name", this.nick);
+    },
+
     handleClick: function(e) {
       e.preventDefault();
 
-      console.log(this.nick);
+      if (!this.isUserLogged()) {
+        axios
+          .post(`http://localhost:8000/user/`, { name: this.nick })
+          .then(res => {
+            console.log(JSON.stringify(res));
+            return res;
+          })
+          .then(res => {
+            this.userAddedOK();
+          })
+          .catch(error => console.log(error));
+      }
     }
   }
-}
+};
 </script>
